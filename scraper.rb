1f2fba0509a85db0cfc88a03b91b089b08b62335
Udo Schmidt Junior <udo.schmidt.jr@gmail.com>
2024-12-31 12:17:02 -0300

    Scraper

--- scraper.rb
+++ scraper.rb
@@ -6,14 +6,16 @@ def get_fundamentus_info(driver, url, filename = "output.csv")
   driver.get(url)
   wait = Selenium::WebDriver::Wait.new(timeout: 15)
   wait.until do
+    puts "dentro wait"
     header = driver.find_elements(css: "thead")
     tbody = driver.find_elements(css: 'tbody[aria-live="polite"]')
     rows = tbody.first.find_elements(css: "tr")
     # puts header.first.text
+    puts "Pegando as info"
 
-    puts driver.page_source
 
     header = header.first.text.gsub("\n", "\t")
+    puts header #{header}
     CSV.open(filename, "w", write_headers: true, headers: header, col_sep: "\t") do |line|
       puts "Criando arquivo"
       rows.each_with_index do |row, index|
@@ -28,8 +30,10 @@ end
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
 driver = Selenium::WebDriver.for :chrome, options: options
 
 get_fundamentus_info driver, "https://fundamentus.com.br/resultado.php?interface=classic&interface=mobile", "acoes.csv"
