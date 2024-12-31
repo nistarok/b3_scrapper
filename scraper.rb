e5a1e4492fe4cc4d36ef478c4d91ab3e2bae8062
Udo Schmidt Junior <udo.schmidt.jr@gmail.com>
2024-12-31 12:12:45 -0300

    Scraper

--- scraper.rb
+++ scraper.rb
@@ -1,37 +1,38 @@
-require 'selenium-webdriver'
-require 'csv'
+require "selenium-webdriver"
+require "csv"
 
-def get_fundamentus_info driver, url, filename = 'output.csv'
+def get_fundamentus_info(driver, url, filename = "output.csv")
   puts "Iniciando arquivo #{filename} da url #{url}"
   driver.get(url)
-  # header = driver.find_elements(css: 'thead')
-  # tbody = driver.find_elements(css: 'tbody[aria-live="polite"]')
-  # rows = tbody.first.find_elements(css: 'tr')
-  # puts header.first.text
+  wait = Selenium::WebDriver::Wait.new(timeout: 15)
+  wait.until do
+    header = driver.find_elements(css: "thead")
+    tbody = driver.find_elements(css: 'tbody[aria-live="polite"]')
+    rows = tbody.first.find_elements(css: "tr")
+    # puts header.first.text
 
-  puts driver.page_source
+    puts driver.page_source
 
-  header = header.first.text.gsub("\n","\t")
-  CSV.open( filename, 'w', write_headers: true, headers: header, col_sep: "\t") do |line|
-    puts "Criando arquivo"
-    rows.each_with_index do |row, index|
-      puts "#{filename} - linha #{index}"
-      line << row.find_elements(css: 'td').map(&:text)
+    header = header.first.text.gsub("\n", "\t")
+    CSV.open(filename, "w", write_headers: true, headers: header, col_sep: "\t") do |line|
+      puts "Criando arquivo"
+      rows.each_with_index do |row, index|
+        puts "#{filename} - linha #{index}"
+        line << row.find_elements(css: "td").map(&:text)
+      end
     end
-  end
 
-  puts "Arquivo gerado #{filename}"
+    puts "Arquivo gerado #{filename}"
+  end
 end
 
-
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
 driver = Selenium::WebDriver.for :chrome, options: options
 
-get_fundamentus_info driver, 'https://fundamentus.com.br/resultado.php?interface=classic&interface=mobile', 'acoes.csv'
-get_fundamentus_info driver, 'https://fundamentus.com.br/fii_resultado.php?interface=classic&interface=mobile', 'fiis.csv'
-
+get_fundamentus_info driver, "https://fundamentus.com.br/resultado.php?interface=classic&interface=mobile", "acoes.csv"
+get_fundamentus_info driver, "https://fundamentus.com.br/fii_resultado.php?interface=classic&interface=mobile", "fiis.csv"
 
 driver.quit
